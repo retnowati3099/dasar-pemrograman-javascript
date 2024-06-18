ac0cac0ed4950cd816f1502f5e31d31d5d7e8c75
Retno Wati <retnowati3099@gmail.com>
2024-06-18 16:02:59 +0700

    tipe data undefined dan numbers

---
+++ tipe-data.js
@@ -0,0 +1,16 @@
+// undefined
+let middleName;
+console.log(typeof middleName); // output: undefined
+
+//  numbers
+let afriskaAge = 26;
+console.log(typeof(afriskaAge));
+const PI = 3.14;
+console.log(typeof(PI));
+
+/*
+- Tipe data adalah pengklasifikasian data berdasarkan jenisnya.
+- Fungsi typeof() digunakan untuk memastikan tipe data pada variabel dengan mengembalikan tipe data tersebut dalam bentuk teks.
+- Tipe data undefined: terbentuk ketika sebuah variabel tidak memiliki nilai. Artinya ketika jika suatu variabel dideklarasikan tanpa diinisialisasi, maka variabel tersebut menjadi undefined.
+- Tipe data number: nilainya adalah angka. Jika angka berupa bilangan desimal, maka bisa menggunakan titik pada pecahan bilangannya.
+*/
