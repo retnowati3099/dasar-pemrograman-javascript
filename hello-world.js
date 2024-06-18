d37cce8c84705f67a56df78b12bebb86553000ce
Retno Wati <retnowati3099@gmail.com>
2024-06-18 15:09:35 +0700

    hello world

--- hello-world.js
+++ hello-world.js
@@ -2,5 +2,5 @@ console.log("Hello World!")
 
 /*
 - console.log() adalah kode bawaan javascript untuk menampilkan pesan ke konsol, bisa berupa web konsol atau konsol terminal/command prompt.
-- Kode atau teks yang berada di dalam anda kurung adalah pesan yang ingin ditampilkan
+- Kode atau teks yang berada di dalam tanda kurung adalah pesan yang ingin ditampilkan. 
 */
