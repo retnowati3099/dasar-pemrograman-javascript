8fd7204973fd53cde42fee63273b9d1d6f56c89d
Retno Wati <retnowati3099@gmail.com>
2024-06-18 20:06:17 +0700

    increment and decrement

---
+++ increment-dan-decrement.js
@@ -0,0 +1,18 @@
+// increment dan decrement
+
+let postfix = 5
+console.log(postfix++) // output: 5
+console.log(postfix) // output: 6
+
+let prefix = 5
+console.log(++prefix); // output: 6
+console.log(prefix); // output: 6
+
+prefix--
+console.log(prefix) // output: 5
+/*
+- Pada operator aritmatika terdapat operator incrment (++) dan decrement (--).
+- Operator increment dan decrement digunakan untuk menambahkan atau mengurangi nilai 1 pada nilai variabel yang ada sekarang.
+- Jika (x++), expession akan menghasilkan nilai variabel sebelum ditingkatkan nilainya.
+- Jika (++x), expression akan menghasilkan nilai variabel setelah ditingkatkan nilainya.
+*/
