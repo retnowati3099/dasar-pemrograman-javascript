808f5480c14ef3470006c901fa8b510b9ac41115
Retno Wati <retnowati3099@gmail.com>
2024-06-18 15:15:30 +0700

    comments

---
+++ comments.js
@@ -0,0 +1,13 @@
+// komentar satu baris
+
+/*
+komentar
+lebih dari
+satu baris
+*/
+
+/*
+- Comments atau komentar adalah instruksi yang ditulis dalam suatu program tetapi tidak dijalankan oleh komputer.
+- Komentar akan dilewatkan oleh interpreter atau compiler.
+- Komentar bisa digunakan sebagai dokumentasi atau penjelasan dari kode yang ditulis.
+*/
