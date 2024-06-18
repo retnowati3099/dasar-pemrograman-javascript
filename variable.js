dd5879af082ffc5a85e05d76fb2ec52e65e94efe
Retno Wati <retnowati3099@gmail.com>
2024-06-18 15:42:22 +0700

    variable

---
+++ variable.js
@@ -0,0 +1,22 @@
+let lastName // deklarasi variabel dengan keyword let
+lastName = "Wati" // inisialisasi variabel (mengisi nilai variabel)
+console.log(lastName);
+
+let firstName = "Retno" 
+console.log("Retno")
+
+const PI = 3.14
+console.log(PI)
+
+/*
+- Variabel umumnya digunakan untuk menyimpan informasi atau nilai yang akan dikelola dalam sebuah program.
+- Pada JavaScript setidaknya ada tiga cara untuk mendeklarasikan sebuah variabel, yaitu menggunakan keyword var, let, dan const.
+- Pada versi ECMAScript 2015(ES6) dikenalkan deklarasi variabel dengan let dan const untuk menggantikan var yang dinilai kontroversial dan rawan menimbulkan bug.
+- Kode untuk menginisialisasi nilai ke dalam variabel dengan tanda sama dengan (=) disebut dengan assignment expression.
+- Beberapa aturan dalam penamaan variabel:
+1. Harus dimulai dengan huruf atau underscore
+2. Dapat terdiri atas huruf, angka, dan underscore dengan berbagai kombinasi
+3. Tidak boleh mengandung spasi (whitespace). Jika penamaan variabel lebih dari dua kata, maka dituliskan secara camelCase
+4. Tidak boleh mengandung karakter spesial (!.,/\+*=dll)
+- Const kependekan dari constant yang digunakan untuk mendeklarasi variabel yang bernilai konstan dan tidak bisa diubah setelah diinisialisasi nilainya.
+*/
