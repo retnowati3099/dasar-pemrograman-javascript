3659c9246f8eb0307e7e0052c69392383fcc45fb
Retno Wati <retnowati3099@gmail.com>
2024-06-18 15:08:31 +0700

    first program for javascript, hello world

---
+++ hello-world.js
@@ -0,0 +1,6 @@
+console.log("Hello World!")
+
+/*
+- console.log() adalah kode bawaan javascript untuk menampilkan pesan ke konsol, bisa berupa web konsol atau konsol terminal/command prompt.
+- Kode atau teks yang berada di dalam anda kurung adalah pesan yang ingin ditampilkan
+*/
