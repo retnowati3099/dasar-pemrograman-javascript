c498e42688244a5b3c8beba887b03820c0e1d096
Retno Wati <retnowati3099@gmail.com>
2024-06-19 07:34:15 +0700

    comparison assignment

--- operator.js
+++ operator.js
@@ -10,6 +10,23 @@ num3 %= num2; // num3 = num3 % num2, num3 = 5
 console.log(num1, num2); // output: 10 10
 console.log(num3);
 
+// comparison operator
+let num4 = 1;
+let num5 = 2;
+console.log(num4 < num5); // output: true
+console.log(num4 > num5); // output: false
+console.log(num4 <= num5); // output: true
+console.log(num4 >= num5); // output: false
+num4 = "1";
+num5 = 1;
+console.log(num4 == num5); // output: true
+console.log(num4 === num5); // output: false
+num4 = "2";
+num5 = 3;
+console.log(num4 != num5); // output: true
+console.log(num4 !== num5); // output: true
+
 /*
 - Operator dalam bahasa pemrograman adalah simbol yang memberi tahu interpreter untuk melakukan operasi seperti matematika, relasional, atau logika untuk memberikan hasil tertentu.
+- "sama" (==) dan "identik" (===). Jika inginmembandingkan hanya dari kesamanaan nilai, maka bisa menggunakan == tetapi jika ingin membandingkan dengan memerhatikan tipe datanya, maka menggunakan ===.
 */
