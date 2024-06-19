27b507b8bb4d366533814acf18b533815c700dac
Retno Wati <retnowati3099@gmail.com>
2024-06-19 07:19:50 +0700

    assignment operator

---
+++ operator.js
@@ -0,0 +1,15 @@
+// assignment operator
+let num1 = 10;
+let num2 = num1; // num2 = 10
+let num3 = 5;
+num3 += num2; // num3 = num3 + num2, num3 = 15
+num3 -= num2; // num3 = num3 - num2, num3 = 5
+num3 *= num2; // num3 = num3 * num2, num3 = 50
+num3 /= num2; // num3 = num3 / num2, num3 = 5
+num3 %= num2; // num3 = num3 % num2, num3 = 5
+console.log(num1, num2); // output: 10 10
+console.log(num3);
+
+/*
+- Operator dalam bahasa pemrograman adalah simbol yang memberi tahu interpreter untuk melakukan operasi seperti matematika, relasional, atau logika untuk memberikan hasil tertentu.
+*/
